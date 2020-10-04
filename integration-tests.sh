f070ded798baa26cf0cba721c98aa40ba729546d
Chris Lewis <chris.lewis2@gmail.com>
2020-10-04 14:48:13 +1100

    wip

--- integration-tests.sh
+++ integration-tests.sh
@@ -7,12 +7,3 @@ else
     echo "FAIL: expected ($expected) got ($output)"
     exit 1
 fi
-
-expected="Exception in resolve-symbol. Missing reference: missing-function"
-output=$((echo "(missing-function 1 2)\n(exit)") | $SCHEME_COMMAND --script repl.scm 2>&1 > /dev/null)
-if [ "$output" = "$expected" ]; then
-    echo "PASS: exception handling behaves as expected"
-else
-    echo "FAIL: expected ($expected) got ($output)"
-    exit 1
-fi
