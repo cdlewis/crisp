0b9ad87bf0a127a0f1dc9445ee8b1a072cf1ece0
Chris Lewis <chris.lewis2@gmail.com>
2020-10-04 14:50:43 +1100

    wip

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,9 +1,3 @@
 expected="> 2
 > "
 output=$((echo "(+ 1 1)\n(exit)") | $SCHEME_COMMAND --script repl.scm)
-if [ "$output" = "$expected" ]; then
-    echo "PASS: repl behaves as expected"
-else
-    echo "FAIL: expected ($expected) got ($output)"
-    exit 1
-fi
